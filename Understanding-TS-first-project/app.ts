afc641b23936b67f0f81ce3cd0cdfb7a6b23db67
acc1iela <acc1iela@gmail.com>
2023-02-16 02:05:19 +0900

    update: enum型について

--- Understanding-TS-first-project/app.ts
+++ Understanding-TS-first-project/app.ts
@@ -1,17 +1,35 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'accio',
+//   age: 30,
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+
+const person = {
   name: 'accio',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author'],
+  role: Role.ADMIN,
 };
 
-console.log(person);
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('読み取り専用ユーザー');
 }
