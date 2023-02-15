28ed53bbb695c32088902337cd43d96d6add30b2
acc1iela <acc1iela@gmail.com>
2023-02-16 01:43:16 +0900

    update: array型について

--- Understanding-TS-first-project/app.ts
+++ Understanding-TS-first-project/app.ts
@@ -1,6 +1,11 @@
 const person = {
   name: 'accio',
   age: 30,
+  hobbies: ['Sports', 'Cooking'],
 };
 
 console.log(person);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
