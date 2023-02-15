638c7eec39ece05626b09452ae39f29f6d5bfdc6
acc1iela <acc1iela@gmail.com>
2023-02-16 01:10:34 +0900

    update: 型について

---
+++ Understanding-TS-first-project/app.js
@@ -0,0 +1,7 @@
+function add1(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var result = add1(number1, number2);
+console.log(result);
---
+++ Understanding-TS-first-project/app.ts
@@ -0,0 +1,9 @@
+function add1(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add1(number1, number2);
+console.log(result);
