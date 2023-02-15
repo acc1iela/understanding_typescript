46bfe0a460923064cc1e531c92b86ea13fcbfaed
acc1iela <acc1iela@gmail.com>
2023-02-16 01:38:55 +0900

    update: 型推論について

--- Understanding-TS-first-project/app.ts
+++ Understanding-TS-first-project/app.ts
@@ -1,9 +1,6 @@
-function add1(n1: number, n2: number) {
-  return n1 + n2;
-}
+const person = {
+  name: 'accio',
+  age: 30,
+};
 
-const number1 = 5;
-const number2 = 2.8;
-
-const result = add1(number1, number2);
-console.log(result);
+console.log(person);
