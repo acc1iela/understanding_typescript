32b2a152b86c553346bd8bbc63cd68ad0fc99f63
acc1iela <acc1iela@gmail.com>
2023-02-17 01:46:49 +0900

    update: 型エイリアスについて

--- Understanding-TS-first-project/app.ts
+++ Understanding-TS-first-project/app.ts
@@ -1,3 +1,6 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
 // const person: {
 //   name: string;
 //   age: number;
@@ -35,7 +38,7 @@ if (person.role === Role.ADMIN) {
 }
 
 // union型について
-function combine(input1: number | string, input2: number | string) {
+function combine(input1: Combinable, input2: Combinable) {
   if (typeof input1 === 'number' && typeof input2 === 'number') {
     result = input1 + input2;
   } else {
