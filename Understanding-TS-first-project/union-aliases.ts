1fb71b83c7c3f80bb4a83547246e0acb8a3ece28
acc1iela <acc1iela@gmail.com>
2023-02-18 16:42:39 +0900

    updtae: function型について

--- Understanding-TS-first-project/union-aliases.ts
+++ Understanding-TS-first-project/union-aliases.ts
@@ -6,4 +6,12 @@ function printResult(num: number) {
   console.log('Result: ' + num);
 }
 
+// let combineValues: Function;
+// 型の定義だけ一致していれば良い
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8, 8));
+
 printResult(add(5, 12));
