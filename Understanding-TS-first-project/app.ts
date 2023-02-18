62decbd0bae980effaff226aa8eda7b9aebbf18a
acc1iela <acc1iela@gmail.com>
2023-02-18 17:06:32 +0900

    update: never型について

--- Understanding-TS-first-project/app.ts
+++ Understanding-TS-first-project/app.ts
@@ -54,3 +54,9 @@ let userName: string;
 
 userInput = 5;
 userInput = 'Max';
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError('エラーが発生しました', 500);
