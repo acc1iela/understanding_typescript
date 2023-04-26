0e939f0681cefb3a85b562bdf7e7415b2890b511
acc1iela <acc1iela@gmail.com>
2023-04-27 03:53:16 +0900

    update: tsconfig再インストール

--- Understanding-TS-first-project/app.js
+++ Understanding-TS-first-project/app.js
@@ -1,7 +1,38 @@
-function add1(n1, n2) {
-    return n1 + n2;
+"use strict";
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'accio',
+//   age: 30,
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author'],
+// };
+const person = {
+    name: 'accio',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN,
+};
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var result = add1(number1, number2);
-console.log(result);
+let userInput;
+let userName;
+userInput = 5;
+userInput = 'Max';
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('エラーが発生しました', 500);
--- Understanding-TS-first-project/union-aliases.js
+++ Understanding-TS-first-project/union-aliases.js
@@ -1,3 +1,4 @@
+"use strict";
 function sumple(n1, n2) {
     return n1 + n2;
 }
@@ -6,14 +7,14 @@ function printResult(num) {
 }
 // let combineValues: Function;
 // 型の定義だけ一致していれば良い
-var combineValues;
+let combineValues;
 combineValues = sumple;
 console.log(combineValues(8, 8));
 printResult(sumple(5, 12));
 function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
+    const result = n1 + n2;
     cb(result);
 }
-addAndHandle(10, 20, function (result) {
+addAndHandle(10, 20, (result) => {
     console.log(result);
 });
