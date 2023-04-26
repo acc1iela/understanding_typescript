21ff1107043051420632a4c7b3fa9965adc8129b
acc1iela <acc1iela@gmail.com>
2023-04-27 03:46:02 +0900

    update: tsconfig追記

--- Understanding-TS-first-project/app.ts
+++ Understanding-TS-first-project/app.ts
@@ -33,22 +33,6 @@ for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
 }
 
-if (person.role === Role.ADMIN) {
-  console.log('読み取り専用ユーザー');
-}
-
-// union型について
-function combine(input1: Combinable, input2: Combinable) {
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-}
-
-const combinedAges = combine(30, 26);
-
 let userInput: unknown;
 let userName: string;
 
